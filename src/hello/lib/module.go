c0b45110fe2e40b7c3f2608887f631753076facb
Peter Stenger <peter.a.stenger@gmail.com>
2016-01-25 15:30:58 -0500

    save

--- src/hello/lib/module.go
+++ src/hello/lib/module.go
@@ -2,10 +2,25 @@ package lib
 
 import (
 	"fmt"
+	c "github.com/skilstak/go/colors"
 )
 
-func ImportLine() string {
-	var Statement = "I was imported"
-	fmt.Println(Statement)
-	return Statement
+func PrintNormal(message string) {
+	fmt.Println(message)
+}
+func FillScreen(message string) {
+	for {
+		fmt.Print(c.Rc() + message + " ")
+	}
+}
+func PrintForever(message string) {
+	for {
+		fmt.Println(c.Clear + c.Multi(message))
+	}
+}
+func PrintMulti(message string) {
+	fmt.Println(c.Multi(message) + c.X)
+}
+func PrintRandom(message string) {
+	fmt.Println(c.Rc() + message + c.X)
 }
--- src/hello/main.go
+++ src/hello/main.go
@@ -3,51 +3,11 @@ package main
 import (
 	"fmt"
 	//c  is basically importing it as c then go get
-	c "github.com/skilstak/go/colors"
-	i "github.com/skilstak/go/input"
 	m "hello/lib"
 	"os"
 	s "strings"
 )
 
-func getNameExample(title string) string {
-	//first parens are in, second is out. If only one paren then it's only out.
-	//don't need := if variable already defined, use =
-	name := "Peter"
-	//:= infers the type: string, number
-	return title + " " + name
-}
-func printNormal(message string) {
-	fmt.Println(message)
-}
-func fillScreen(message string) {
-	for {
-		fmt.Print(c.Rc() + message + " ")
-	}
-}
-func printForever(message string) {
-	for {
-		fmt.Println(c.Clear + c.Multi(message))
-	}
-}
-func printMulti(message string) {
-	fmt.Println(c.Multi(message) + c.X)
-}
-func printRandom(message string) {
-	fmt.Println(c.Rc() + message + c.X)
-}
-func userInput() {
-	input, err := i.Prompt(c.Rc() + "--> " + c.X)
-	if err != nil {
-		panic(err)
-	}
-	if s.Contains(input, "hi") {
-		fmt.Println(c.Rc() + "Hello!" + c.X)
-	} else {
-		fmt.Println("")
-	}
-
-}
 func main() {
 	name := "World"
 	option := ""
@@ -55,25 +15,21 @@ func main() {
 		name = os.Args[1]
 		option = os.Args[2]
 		if s.HasPrefix(option, "-f") {
-			printForever("Hello " + name)
+			m.PrintForever("Hello " + name)
 		} else if s.HasPrefix(option, "-r") {
-			printRandom("Hello " + name)
+			m.PrintRandom("Hello " + name)
 			fmt.Println("No")
 		} else if s.HasPrefix(option, "-m") {
-			printMulti("Hello " + name)
+			m.PrintMulti("Hello " + name)
 		} else if s.HasPrefix(option, "-s") {
-			fillScreen("Hello " + name)
+			m.FillScreen("Hello " + name)
 		} else {
-			printNormal("Hello " + name)
+			m.PrintNormal("Hello " + name)
 		}
 	} else if len(os.Args) > 1 {
-		fmt.Println("Add a space then -f,-r,-m,or -s to have more fun!")
 		name = os.Args[1]
-		printNormal("Hello " + name)
+		m.PrintNormal("Hello " + name)
 	} else {
-		fmt.Println("Add a space and then your name to have more fun!")
-		printNormal("Hello " + name)
+		m.PrintNormal("Hello " + name)
 	}
-	m.ImportLine()
-	fmt.Println(Statement)
 }
