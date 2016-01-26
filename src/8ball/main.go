77c66216d0332ed1dcdbe72eaa5daad7751e1af4
Peter Stenger <peter.a.stenger@gmail.com>
2016-01-26 14:04:19 -0500

    save

--- src/8ball/main.go
+++ src/8ball/main.go
@@ -1,30 +1,16 @@
 package main
 
 import (
+	m "8ball/lib"
 	"fmt"
 	h "github.com/skilstak/go/choice"
 	c "github.com/skilstak/go/colors"
 	i "github.com/skilstak/go/input"
+	s "strings"
 )
 
 func main() {
-	welcome := `
-	WELCOME TO THE MAGIC
-              _.a$$$$$a._
-            ,$$$$$$$$$$$$$.
-          ,$$$$$$$$$$$$$$$$$.
-         d$$$$$$$$$$$$$$$$$$$b
-        d$$$$$$$$~'"'~$$$$$$$$b
-       ($$$$$$$p   _   q$$$$$$$)
-       $$$$$$$$   (_)   $$$$$$$$
-       $$$$$$$$   (_)   $$$$$$$$
-       ($$$$$$$b       d$$$$$$$)
-        q$$$$$$$$a._.a$$$$$$$$p
-         q$$$$$$$$$$$$$$$$$$$p
-          '$$$$$$$$$$$$$$$$$'
-            '$$$$$$$$$$$$$'
-	      '~$$$$$$$~'      BALL`
-	fmt.Println(welcome)
+	m.PrintWelcome()
 	var myArray = []string{
 		"Yes",
 		"No",
@@ -34,8 +20,16 @@ func main() {
 		"I don't know...",
 	}
 	for {
-		i.Prompt(c.Rc() + "Ask your question please > " + c.X)
-		item := h.Choice(myArray)
-		fmt.Println(c.Rc() + c.CL + item + c.X)
+		question, err := i.Prompt(c.Rc() + "--> ")
+		if err != nil {
+			panic(err)
+		} else {
+			if s.Contains(question, "die") {
+				fmt.Println("let's not talk about death...")
+			} else {
+				randomItem := h.Choice(myArray)
+				fmt.Println(c.Rc() + c.CL + randomItem + c.X)
+			}
+		}
 	}
 }
