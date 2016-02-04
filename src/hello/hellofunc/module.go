342d83f3b3980a3940c82e2185996d6be54567ac
Peter Stenger <peter.a.stenger@gmail.com>
2016-02-03 20:17:34 -0500

    save

--- src/hello/hellofunc/module.go
+++ src/hello/hellofunc/module.go
@@ -1,4 +1,4 @@
-package lib
+package hellofunc
 
 import (
 	"fmt"
--- src/hello/main.go
+++ src/hello/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	//c  is basically importing it as c then go get
-	m "hello/lib"
+	m "hello/hellofunc"
 	"os"
 	s "strings"
 )
@@ -22,11 +22,34 @@ func main() {
 		} else if s.HasPrefix(option, "-s") {
 			m.FillScreen("Hello " + name)
 		} else {
-			m.PrintNormal("Hello " + name)
+			option = os.Args[1]
+			name = os.Args[2]
+			if s.HasPrefix(option, "-f") {
+				m.PrintForever("Hello " + name)
+			} else if s.HasPrefix(option, "-r") {
+				m.PrintRandom("Hello " + name)
+			} else if s.HasPrefix(option, "-m") {
+				m.PrintMulti("Hello " + name)
+			} else if s.HasPrefix(option, "-s") {
+				m.FillScreen("Hello " + name)
+			} else {
+				m.PrintNormal("Hello " + name)
+			}
 		}
 	} else if len(os.Args) > 1 {
-		name = os.Args[1]
-		m.PrintNormal("Hello " + name)
+		option = os.Args[1]
+		if s.HasPrefix(option, "-f") {
+			m.PrintForever("Hello " + name)
+		} else if s.HasPrefix(option, "-r") {
+			m.PrintRandom("Hello " + name)
+		} else if s.HasPrefix(option, "-m") {
+			m.PrintMulti("Hello " + name)
+		} else if s.HasPrefix(option, "-s") {
+			m.FillScreen("Hello " + name)
+		} else {
+			name = os.Args[1]
+			m.PrintNormal("Hello " + name)
+		}
 	} else {
 		m.PrintNormal("Hello " + name)
 	}
