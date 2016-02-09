ba421c7642eadae07420c1eaea599dd8af1eb2fa
Peter Stenger <peter.a.stenger@gmail.com>
2016-02-09 17:04:44 -0500

    save

---
+++ src/testing/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func printType(x interface{}) bool {
+	stringX := false
+	switch v := x.(type) {
+	case int:
+		fmt.Println("int data", v)
+	case string:
+		stringX = true
+		fmt.Println("string data", v)
+	default:
+		fmt.Println("I don't like your type", v)
+	}
+	return stringX
+}
+func main() {
+	var x string = "hello"
+	printType(x)
+	if isString == true {
+		fmt.Println("It's a string!")
+	}
+}
