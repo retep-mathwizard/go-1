004555db226293c2536d0b950c67806ab6a973d2
Peter Stenger <peter.a.stenger@gmail.com>
2016-02-16 17:42:07 -0500

    save

--- src/battle/mage.go
+++ src/battle/mage.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/retep-mathwizard/utils/
+	i "github.com/retep-mathwizard/utils/src/input"
 )
 
 type class struct {
@@ -11,10 +11,10 @@ type class struct {
 
 func main() {
 	mage := class{health: 200, agility: 50, damage: 80, accuracy: 70}
-	ogre := class{health: 450,agility:25,damage:100,accuracy:40}
-	dodger := class{health: 300,agility:100,damage:40,accuracy:100}
-	soldier := class{health: 400,agility:75,damage:60,accuracy:80}
-	
+	ogre := class{health: 450, agility: 25, damage: 100, accuracy: 40}
+	dodger := class{health: 300, agility: 100, damage: 40, accuracy: 100}
+	soldier := class{health: 400, agility: 75, damage: 60, accuracy: 80}
+	userclass := i.StringInput("What class? > ")
 	mage.health = 10
 	fmt.Println(mage.health)
 }
