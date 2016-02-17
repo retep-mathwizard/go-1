bbb5ffa3bfa021eca22d50993e75fa233fbccfc1
Peter Stenger <peter.a.stenger@gmail.com>
2016-02-17 16:13:40 -0500

    save

--- src/battle/mage.go
+++ src/battle/mage.go
@@ -5,6 +5,7 @@ import (
 	i "github.com/retep-mathwizard/utils/src/input"
 	c "github.com/skilstak/go/colors"
 	"os"
+	"reflect"
 )
 
 //%s = colors, %f = strings
@@ -19,21 +20,25 @@ func getname() (string, string) {
 	fmt.Println(c.CL)
 	return p1name, p2name
 }
-func getclass(p1name string, p2name string) (string, string) {
+func getclass(p1name string, p2name string) (main.class, main.class) {
+	wizard := class{health: 200, agility: 50, damage: 80, accuracy: 70}
+	ogre := class{health: 450, agility: 25, damage: 100, accuracy: 40}
+	archer := class{health: 300, agility: 100, damage: 40, accuracy: 100}
+	soldier := class{health: 400, agility: 75, damage: 60, accuracy: 80}
 	var sentenceA string = p1name + ",what class? (ogre, wizard, soldier, archer) > "
 	var sentenceB string = p2name + " ,what class? (ogre, wizard, soldier, archer) > "
+	p1class := soldier
+	p2class := soldier
 	userclass := i.StringInput(sentenceA)
-	var p1class string
-	var p2class string
 	switch userclass {
-	case "mage":
-		p1class = "mage"
+	case "archer":
+		p1class = archer
 	case "ogre":
-		p1class = "ogre"
+		p1class = ogre
 	case "wizard":
-		p1class = "wizard"
+		p1class = wizard
 	case "solider":
-		p1class = "soldier"
+		p1class = soldier
 	default:
 		fmt.Println("invalid case.")
 		os.Exit(-1)
@@ -41,33 +46,29 @@ func getclass(p1name string, p2name string) (string, string) {
 	fmt.Println(c.CL)
 	userclass2 := i.StringInput(sentenceB)
 	switch userclass2 {
-	case "mage":
-		p2class = "mage"
+	case "archer":
+		p2class = archer
 	case "ogre":
-		p2class = "ogre"
+		p2class = ogre
 	case "wizard":
-		p2class = "wizard"
+		p2class = wizard
 	case "solider":
-		p2class = "soldier"
+		p2class = soldier
 	default:
 		fmt.Println("invalid case.")
 		os.Exit(-1)
 	}
 	fmt.Println(c.CL)
-	return p1class, p2class
+	fmt.Println(reflect.TypeOf(wizard))
+	fmt.Println(p2class)
+	fmt.Println(p1class)
 }
 
 func main() {
 	p1name, p2name := getname()
-	p1class, p2class := getclass(p1name, p2name)
-	/*
-		wizard := class{health: 200, agility: 50, damage: 80, accuracy: 70}
-		ogre := class{health: 450, agility: 25, damage: 100, accuracy: 40}
-		archer := class{health: 300, agility: 100, damage: 40, accuracy: 100}
-		soldier := class{health: 400, agility: 75, damage: 60, accuracy: 80}
-	*/
-	fmt.Println(p1class)
-	fmt.Println(p2class)
+	getclass(p1name, p2name)
+	//fmt.Println(p1class)
+	//fmt.Println(p2class)
 	fmt.Println(p1name)
 	fmt.Println(p2name)
 }
