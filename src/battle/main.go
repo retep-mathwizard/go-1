9d5d2622c1d933541262526cc07f40b7364d8a9e
Peter Stenger <peter.a.stenger@gmail.com>
2016-02-17 20:21:40 -0500

    save

--- src/battle/main.go
+++ src/battle/main.go
@@ -9,10 +9,6 @@ import (
 )
 
 //%s = colors, %f = strings
-type class struct {
-	health, agility, damage, accuracy int
-}
-
 func getname() (string, string) {
 	p1name := i.StringInput("Player 1, what is your name? > ")
 	fmt.Println(c.CL)
@@ -20,11 +16,79 @@ func getname() (string, string) {
 	fmt.Println(c.CL)
 	return p1name, p2name
 }
+
+//*******************************************************//
+type moves struct {
+	name1 string
+	damage1 int
+	accuracy2 int
+	name2 string
+	damage2 int
+	accuracy2 int
+}
+type class struct {
+	moves struct,
+	health, agility, shield int
+       }
+
+wizard := class{
+health: 200,
+agility: 50,
+shield: 80, 
+attacks: moves{
+	name1:"fireball"
+	damage1:50
+	accuracy1:75
+	name2:"lightning"
+	damage2:125
+	accuracy2:25
+	}
+}
+//
+
+ogre := class{
+health: 450,
+agility: 25,
+shield: 10,
+attacks: moves{
+	name1:"HULK SMASH"
+	damage1:500
+	accuracy1:10
+	name2:"club whack"
+	damage2:50
+	accuracy2:25
+	}
+}
+//
+archer := class{
+health: 300,
+agility: 100,
+shield: 40,
+attacks: moves{
+	name1:"sniper"
+	damage1:25
+	accuracy1:100
+	name2:"supa-arrow"
+	damage2:50
+	accuracy2:50
+	}
+}
+//
+soldier := class{
+health: 400,
+agility: 75,
+shield: 60,
+attacks: moves{
+	name1:"broadswipe"
+	damage1:50
+	accuracy1:70
+	name2:"X stroke"
+	damage2:20
+	accuracy2:100
+	}
+}
+/////////////////////////
 func getclass(p1name string, p2name string) (class, class) {
-	wizard := class{health: 200, agility: 50, damage: 80, accuracy: 70}
-	ogre := class{health: 450, agility: 25, damage: 100, accuracy: 40}
-	archer := class{health: 300, agility: 100, damage: 40, accuracy: 100}
-	soldier := class{health: 400, agility: 75, damage: 60, accuracy: 80}
 	var sentenceA string = p1name + ",what class? (ogre, wizard, soldier, archer) > "
 	var sentenceB string = p2name + " ,what class? (ogre, wizard, soldier, archer) > "
 	p1class := soldier
