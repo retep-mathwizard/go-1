444efbb02f57034306d8b5c9323c4eb1c6e6e7a6
Peter Stenger <peter.a.stenger@gmail.com>
2016-02-19 18:51:30 -0500

    save

--- src/json/main.go
+++ src/json/main.go
@@ -16,14 +16,18 @@ func main() {
 	// read the file, using the ioutil pacakge  https://golang.org/pkg/io/ioutil/#ReadFile
 	bytes, err := ioutil.ReadFile("file.json")
 	if err != nil {
+		fmt.Println("There was an error reading the file, make sure it is in the same directory as main.go, or you have the correct path stated")
 		fmt.Println(err)
 		panic(err)
-		// failed to read file, do something with err
 	}
 	honda := &car{}
 	err = json.Unmarshal(bytes, honda)
+	//decodes it
 	if err != nil {
 		// error unmarshalling the json, do something
+		fmt.Println("There was an error decoding the file: make sure you have a correct struct formatting, and uppercase letters")
+		fmt.Println(err)
+		panic(err)
 	}
 	fmt.Println(honda.Name)
 	fmt.Println(honda.Wheels)
