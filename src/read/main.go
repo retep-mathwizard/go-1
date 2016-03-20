0b81efa653b3e7b2f9e0b114fd7ba68d74c9401d
Peter Stenger <peter.a.stenger@gmail.com>
2016-03-20 19:36:36 -0400

    save

--- src/read/main.go
+++ src/read/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 )
 
-func main() {
-	info, err := ioutil.ReadFile("data")
+func getSlice(file string) []string {
+	data, err := ioutil.ReadFile(file)
 	if err != nil {
+		fmt.Println(err)
 		panic(err)
 	}
-	s := string(info)
-	fmt.Println(s)
+	var slice []string
+	err = json.Unmarshal(data, &slice)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	return slice
+}
+func main() {
+	slice := getSlice("data.json")
+	fmt.Println(slice)
 }
