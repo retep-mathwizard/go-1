1d35039c1bc2bbd09e7659b30814ff51b955d474
Peter Stenger <peter.a.stenger@gmail.com>
2016-04-03 20:44:30 -0400

    save

--- src/oldheroes/heroes/main.go
+++ src/oldheroes/heroes/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"github.com/retep-mathwizard/fun/go/src/heroes/bmath"
-	"github.com/retep-mathwizard/fun/go/src/heroes/engine"
-	"github.com/retep-mathwizard/fun/go/src/heroes/info"
-	"github.com/retep-mathwizard/fun/go/src/heroes/load"
 	o "github.com/retep-mathwizard/utils/other"
+	"oldheroes/bmath"
+	"oldheroes/engine"
+	"oldheroes/info"
+	"oldheroes/load"
 	//c "github.com/skilstak/go/colors"
 )
 
@@ -13,7 +13,7 @@ func main() {
 	p1Name, p2Name := info.GetName()
 	//name
 	p1Class := load.LoadClass("../data/class.json")
-	#ai#p2Class := load.LoadClass("../data/class2.json")
+	p2Class := load.LoadClass("../data/class2.json")
 	//class selection
 	p2HasShield := false
 	p2MovePos := 1
@@ -28,7 +28,7 @@ func main() {
 		o.Spacer(3)
 		//attack
 		//remove the shield, turn is over
-		#ai#p2MovePos, p2HasShield = engine.ChooseMove(p2Name, p2Class)
+		p2MovePos, p2HasShield = engine.ChooseMove(p2Name, p2Class)
 		o.Spacer(3)
 		//p1class with the name of p1name attacks p2class with the name of p2name and the health of p2health using move #movePos, damage is reduced if p2 has a shield.
 		p1Health = engine.Attack(p2Class, p2Name, p1Class, p1Name, p1Health, p2MovePos, p1HasShield)
