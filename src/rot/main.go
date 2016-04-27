55162ffb9203b96e5b14e80a86ff3c574131e576
Peter Stenger <peter.a.stenger@gmail.com>
2016-04-26 21:07:05 -0400

    save

---
+++ src/rot/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func rot(number int, str string) string {
+	caps := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	lower := "abcdefghijklmnopqrstuvwxyz"
+	for pos, char := range str {
+		strchar := string(char)
+		if strings.Contains(caps, strchar) {
+			index := strings.Index(caps, strchar)
+			str = str[:pos] + string(caps[(index+number)%26]) + str[pos+1:]
+		} else if strings.Contains(lower, strchar) {
+			index := strings.Index(lower, strchar)
+			str = str[:pos] + string(lower[(index+number)%26]) + str[pos+1:]
+		}
+	}
+	return str
+}
+
+func main() {
+	fmt.Println(rot(13, "ATTACK AT DAWN"))
+}
