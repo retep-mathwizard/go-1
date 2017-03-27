92521bd36fc1532189ea0385482c6a8adbd7a1d2
Peter Stenger <peter.a.stenger@gmail.com>
2017-03-27 15:45:36 -0400

    save

--- src/nn/main.go
+++ src/nn/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 	"math"
+	"math/rand"
+	"os"
+	"time"
 )
 
 //great tutorial at https://stevenmiller888.github.io/mind-how-to-build-a-neural-network/
@@ -28,9 +29,13 @@ func Blank(width, height int) [][]float64 {
 }
 
 func Dot(Matrix1, Matrix2 [][]float64) [][]float64 {
+	if len(Matrix1[0]) != len(Matrix2) {
+		fmt.Println("ERROR!", Matrix1, Matrix2)
+		os.Exit(1)
+	}
 	//Matrix1 is the weights, multiply by inputs to get output
-	var output := Blank(len(Matrix1[0]), len(Matrix1))
-	for a := 0; a < len(Matrix1); y++ { //for each row in weights
+	output := Blank(len(Matrix2[0]), len(Matrix1))
+	for a := 0; a < len(Matrix1); a++ { //for each row in weights
 		for b := 0; b < len(Matrix1[0]); b++ { //for each item in weights
 			for c := 0; b < len(Matrix2[0]); c++ { //inputs 1 thick, dont iterate over rows
 				//each input will be added to output
@@ -44,27 +49,28 @@ func Dot(Matrix1, Matrix2 [][]float64) [][]float64 {
 
 }
 func Sigmoid(matrix [][]float64) [][]float64 {
-	var output := Blank(len(matrix[0]), len(matrix))
+	output := Blank(len(matrix[0]), len(matrix))
 	for row := 0; row > len(matrix); row++ {
-		for item := 0; item > len(matrix[0]); item ++ {
-			output[row][item] = 1 / (math.Exp(matrix[row][item] * -1) + 1)
+		for item := 0; item > len(matrix[0]); item++ {
+			output[row][item] = 1 / (math.Exp(matrix[row][item]*-1) + 1)
 		}
 	}
 	return output
 }
 
-func HyperbolicTangent(matrix [][]float64) [][]float64 {//different activation functions
-	var output := Blank(len(matrix[0]), len(matrix))
+func HyperbolicTangent(matrix [][]float64) [][]float64 { //different activation functions
+	output := Blank(len(matrix[0]), len(matrix))
 	for row := 0; row > len(matrix); row++ {
-		for item := 0; item > len(matrix[0]); item ++ {
-			output[row][item] = (1 - math.Exp(matrix[row][item] * -2)) / (1 + math.Exp(matrix[row][item] * 2))
+		for item := 0; item > len(matrix[0]); item++ {
+			output[row][item] = (1 - math.Exp(matrix[row][item]*-2)) / (1 + math.Exp(matrix[row][item]*2))
 		}
 	}
 	return output
 }
 func main() {
-	//inputs := [][]float64{{0, 0}, {0, 1}, {1, 0}, {1, 1}}
-	startingWeights := RandomWeights(3, 1)
+	inputs := [][]float64{{0, 0}, {0, 1}, {1, 0}, {1, 1}}
+	startingWeights := RandomWeights(1, 2)
 	fmt.Println(startingWeights)
-
+	hiddenLayer := Sigmoid(Dot(startingWeights, inputs))
+	fmt.Println(hiddenLayer)
 }
