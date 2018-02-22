790d3e2f4535417af49a4ffe6a282e5253d0f858
Peter Stenger <peter.a.stenger@gmail.com>
2018-02-21 21:29:05 -0500

    save

--- tictac/tictac.go
+++ tictac/tictac.go
@@ -17,7 +17,7 @@ func join_ascii(rletter1, rletter2, rletter3, rcol string) string {
 	for i := 0; i < len(letter1); i++ {
 		result += letter1[i] + col[i] + letter2[i] + col[i] + letter3[i] + "\n"
 	}
-	return result[:len(result)-2]
+	return result[:len(result)-1]
 }
 func drawboard(board [][]int) {
 	for i, row := range board {
@@ -32,7 +32,7 @@ var letters = map[int]string{2: "\\/\n/\\", 1: "/\\\n\\/", 0: "  \n  "}
 var line = "--+--+--"
 var col = "|\n|"
 var winning_combos = [][]string{{"11", "12", "13"}, {"21", "22", "23"}, {"31", "32", "33"}, {"11", "21", "31"}, {"12", "22", "32"}, {"13", "23", "33"}, {"11", "22", "33"}, {"13", "22", "31"}}
-var board = [][]int{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}
+var board = [][]int{{1, 0, 2}, {0, 1, 2}, {0, 0, 1}}
 
 func main() {
 	drawboard(board)
